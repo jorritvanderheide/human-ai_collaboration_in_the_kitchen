0d5f1f1f1a0ec53f397f57326e7fe5f15e6014d0
jorritvanderheide <l3lockbuster@protonmail.com>
2020-11-30 16:32:33 +0100

    Update script

--- js/script.js
+++ js/script.js
@@ -1,66 +1,67 @@
 // variables
 let ingredientData;
-let ingredientDataUrl =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRghrGRbuEdWP5J_FPzh5f-oNdPnsGpc8hkgPe3k7o5VliWepVoAWIgZVsaI0At4M_b4yTIYEe7F-rX/pub?gid=0&single=true&output=csv";
+let ingredientDataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRghrGRbuEdWP5J_FPzh5f-oNdPnsGpc8hkgPe3k7o5VliWepVoAWIgZVsaI0At4M_b4yTIYEe7F-rX/pub?gid=0&single=true&output=csv";
 let recipeData;
-let recipeDataUrl =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSIGYNRFDYXKj00RPTS5pbpAr9if65t2kbi2In5_dkUwzMPO2HVV4QvIxpfxFq0jo7fNHB7HG7Z3Nvf/pub?gid=0&single=true&output=csv";
+let recipeDataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSIGYNRFDYXKj00RPTS5pbpAr9if65t2kbi2In5_dkUwzMPO2HVV4QvIxpfxFq0jo7fNHB7HG7Z3Nvf/pub?gid=0&single=true&output=csv";
 
-// // setup nn
-// const options = {
-//   input: 1,
-//   outputs: 2,
-//   task: 'regression',
-//   // debug: true
-// }
+let trainingData;
+let trainingDataUrl = '';
+
+let recipeVector = [];
+let newRecipeVector = [];
+
+// setup neural network
+let model;
+let options = {
+  inputs = 60,
+  outputs = 1,
+  task = 'regression'
+};
 
 // setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // noLoop();
+
+  // setup nn
+  model = ml5.neuralNetwork(options);
 
   // preload data
   ingredientData = loadTable(ingredientDataUrl, "csv", "header");
   recipeData = loadTable(recipeDataUrl, "csv", "header");
+
+  //preload trained model
+  // trainingData =;
 }
 
 // draw
 function draw() {
   background(255);
+
   if (ingredientData && recipeData) {
+
+    // get tryout ingredient from UI
+
+    // get intention filter from UI
     
-    h = 100;
-    textSize(32);
+    loop {
+    // get recipe 1 - 20
 
-    for (i = 0; i < 10; i++) {
-      let value = ingredientData.getColumn('Product_EN');
+    // intention filter
 
-      button = createButton(value[i]);
-      button.position(100, h);
-      h = h + 85;
+    // remove old ingredient
+
+    // generate new recipe vector
+
+    // execute model
+    model.addData(inputs, output)
+
+    // if > threshold -> save recommendation to recipe
+
     }
-    
 
-    // const nn = ml5.neuralNetwork(options);
-
-    // // load trained model
-    // const modelDetails = {
-    //   model: 'model/model.json',
-    //   metadata: 'model/model_meta.json',
-    //   weights: 'model/model.weights.bin'
-    // }
-    // nn.load(modelDetails, modelLoaded)
-
-    // // execute model
-    // function modelLoaded() {
-
-    // }
+    // send saved results to UI
+ 
   }
-}
-
-function changeBG() {
-  let val = random(255);
-  background(val);
 }
 
 // resize canvas on window resize
