1d4817dc51d770763cf966b90f0a39a1b4f8942e
jorritvanderheide <l3lockbuster@protonmail.com>
2020-12-12 19:46:53 +0100

    SetupData corectie

--- js/setupData.js
+++ js/setupData.js
@@ -1,4 +1,3 @@
-
 let recipeData; // full recipe .csv
 let ingredientData; // full ingredient .csv
 let recipeList = []; // array with the names of the recipes
@@ -10,26 +9,29 @@ let ingredientCategory = []; // array with the ingredient category
 
 // import csv's
 let recipeDataUrl =
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vRDKC0TKzDZxYVeogUXlhM14U7CsL3rAX0-q4J_hOO8rj-OWxcaANNLuw5WYEuv7FMBylZGa3GC8Mp7/pub?gid=0&single=true&output=csv';
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRDKC0TKzDZxYVeogUXlhM14U7CsL3rAX0-q4J_hOO8rj-OWxcaANNLuw5WYEuv7FMBylZGa3GC8Mp7/pub?gid=0&single=true&output=csv";
 let ingredientDataUrl =
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_zg-Q_2KiUOTb6ZnWY-GHvp2Pjs7bnfWZ1jpP35EJ0nOEkAUCtoCBgsOzh0VY1KXU8Q_4SSweBDEP/pub?gid=0&single=true&output=csv';
-
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_zg-Q_2KiUOTb6ZnWY-GHvp2Pjs7bnfWZ1jpP35EJ0nOEkAUCtoCBgsOzh0VY1KXU8Q_4SSweBDEP/pub?gid=0&single=true&output=csv";
 
 function setup() {
+  // preload data
+  ingredientData = loadTable(ingredientDataUrl, "csv", "header");
+  recipeData = loadTable(recipeDataUrl, "csv", "header");
+}
 
-    // preload data
-    ingredientData = loadTable(ingredientDataUrl, 'csv', 'header');
-    recipeData = loadTable(recipeDataUrl, 'csv', 'header');
+function draw() {
+  // check if data loaded
+  if (recipeData.getRowCount() && ingredientData.getRowCount()) {
+    // assign variables
+    recipeList = recipeData.getColumn("recipeName");
+    recipeVectors = recipeData.getColumn("recipeVector");
+    ingredientList = ingredientData.getColumn("ingredient");
+    ingredientPlant = ingredientData.getColumn("isPlantbased");
+    ingredientSeason = ingredientData.getColumn("inSeason");
+    ingredientCategory = ingredientData.getColumn("category");
+    noLoop(); // run once when data is loaded
 
-    console.log('im here');
-    console.log(ingredientData.rows.count);
-
-    for (i=0 ; i < recipeData.rows.count; i++) {         // for every recipe get recipe data from columns
-        recipeList[i] = recipeData.getColumn('recipeName');
-        recipeVectors[i] = recipeData.getColumn('recipeVector');
-        ingredientList[i] = ingredientData.getColumn('ingredient');
-        ingredientPlant[i] = ingredientData.getColumn('isPlantbased');
-        ingredientSeason[i] = ingredientData.getColumn('inSeason');
-        ingredientCategory[i] = ingredientData.getColumn('category');  
-    }
+    console.log(recipeList);
+    console.log(recipeList[5]);
+  }
 }
