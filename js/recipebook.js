67e75e8870d31777562ff2a6f6dd85c5f81adf7d
jorritvanderheide <l3lockbuster@protonmail.com>
2020-12-13 21:15:23 +0100

    Merge old

--- js/recipebook.js
+++ js/recipebook.js
@@ -1,12 +1,9 @@
-
 let recipeIngredients = [];
-
 
 $(document).ready(function(){
     // Activate Carousel
     $("#carouselExampleControls").carousel({interval: false});
 });
-
 
 function setupRecipes() {
     console.log(recipeList);
