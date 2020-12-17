eceada7dda4231a2c54ff949ea88435286095158
larsdel <73589306+larsdel@users.noreply.github.com>
2020-12-17 17:05:35 +0100

    oocsi tala

--- js/oocsi.js
+++ js/oocsi.js
@@ -10,16 +10,19 @@ OOCSI.subscribe('Tala', function (msg) {
     var Tala_status = msg.data.Tala_status;
     console.log(Tala_status);
     if (Tala_status) {
+        logAI("*AI: \tThere is enough energy for this recipe!");
     } else if (!Tala_status) {
+        logAI("*AI: \tAnother recipe would be better energywise");
     }
 });
 
 function sendCookInfo() {
+    logAI("*AI: \tI will ask Tala for the energy status!");
     var recipe_energy = Recipe_Energy[currentIndex];
     var recipe_time = Recipe_Time[currentIndex];
     var recipe_name = recipeList[currentIndex];
 
-    var parcel = { energy: recipe_energy, time: recipe_time, name: recipe_name };
+    var parcel = { 'energy': recipe_energy, 'time': recipe_time, 'name': recipe_name };
 
     OOCSI.send('Recuisine', parcel);
     console.log('data send');
